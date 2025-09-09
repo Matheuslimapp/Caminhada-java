11cca7f5895d8d218601111b220f9486e04ed089
Matheus pantaleão <matheuslimapereira990@gmail.com>
2025-09-08 22:58:36 -0300

    Metodos_pt1.java

--- src/Relenbrando_metodos/Metodos_pt1.java
+++ src/Relenbrando_metodos/Metodos_pt1.java
@@ -2,30 +2,33 @@ package Relenbrando_metodos;
 
 import java.util.Random;
 
+//Hello world com metodos
 public class Metodos_pt1 {
     public void helloWorld(){
         System.out.println("Hello world");
     }
+//passagem de parametros com metodos
     public void soma(int num1, int num2){
         int resultado = num1 + num2;
         System.out.println(resultado);
     }
+    //metodos com retorno de valor
     /*public int soma(int num1, int num2){
         int resultado = num1 + num2;
         return resultado;
     }*/
-
+    //metodos com sobre sobrecarga
     public void soma(int num1, int num2, int num3){
         int resultado = num1 + num2 + num3;
         System.out.println(resultado);
     }
-
+    //metodos com static
     public static void randomNumero(){
         Random random = new Random();
         int numeroRandom = random.nextInt(0, 10);
         System.out.println(numeroRandom);
     }
-
+    //Execuçao do metodos
     public static void main(String[] args) {
         Metodos_pt1 metodosPt1 = new Metodos_pt1();
         metodosPt1.helloWorld();
