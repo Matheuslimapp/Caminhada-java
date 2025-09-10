fd98640838ab1d9de46c248b82b65415c67c564e
matheuslimapp <matheuslimapereira990@gmail.co>
2025-09-09 22:22:32 -0300

    Metodos

--- src/Relenbrando_metodos/Metodos_pt2.java
+++ src/Relenbrando_metodos/Metodos_pt2.java
@@ -2,17 +2,20 @@ package Relenbrando_metodos;
 
 public class Metodos_pt2 {
     public String nome;
+    //Método de instância
     public void recebendoNome(String nome){
         this.nome = nome;
         System.out.println("Hello " + nome);
     }
-    public void metodosComArrays(int[] numeros){
+    //metodos utilizando array
+    public void metodosComArray(int[] numeros){
         int soma = 0;
         for (int numero : numeros){
             soma += numero;
         }
         System.out.println(soma);
     }
+    //metodos com args que e COMO fosse um array
     public void metodosComArgs(int... numeros){
         int soma = 0;
         for (int numero : numeros){
@@ -20,12 +23,12 @@ public class Metodos_pt2 {
         }
         System.out.println(soma);
     }
-
+    //Fazendo os teste de metodos
     public static void main(String[] args) {
         Metodos_pt2 metodosPt2 = new Metodos_pt2();
         metodosPt2.recebendoNome("matheus");
         int[] numeros = {100,200,300,400,500};
-        metodosPt2.metodosComArrays(numeros);
+        metodosPt2.metodosComArray(numeros);
         metodosPt2.metodosComArgs(100,200,300,400,500);
     }
 }
