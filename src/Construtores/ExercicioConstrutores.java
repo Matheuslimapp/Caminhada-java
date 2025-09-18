6701ee42d9cc1885315c7cd4c37f8fd76093dbe5
matheuslimapp <matheuslimapereira990@gmail.co>
2025-09-17 21:44:04 -0300

    Metodos

--- src/Construtores/ExercicioConstrutores.java
+++ src/Construtores/ExercicioConstrutores.java
@@ -3,6 +3,7 @@ package Construtores;
 import org.w3c.dom.ls.LSOutput;
 
 public class ExercicioConstrutores {
+    // Atributos dos construtores
     String titular;
     double saldo;
     double limite ;
@@ -10,10 +11,16 @@ public class ExercicioConstrutores {
 
 
     public void imprimir(){
-        System.out.println("Titular: " + this.titular + ", tipo da conta: " + this.tipoConta + ", Saldo: " + this.saldo + ", Limite da conta: " +  this.limite );
+        // Exibe no console todas as informações da contas
+        System.out.println(
+                "Titular: " + this.titular
+                + ", tipo da conta: " + this.tipoConta
+                + ", Saldo: " + this.saldo + ", Limite da conta: "
+                +  this.limite
+        );
     }
 
-
+    // Construtor 1 - só com titular
     public ExercicioConstrutores(String titular){
         this.titular = titular;
         this.saldo = 0;
@@ -22,6 +29,7 @@ public class ExercicioConstrutores {
         imprimir();
     }
 
+    // Construtor 2 - titular e tipoConta
     public ExercicioConstrutores(String titular, String tipoConta) {
         this.titular = titular;
         this.tipoConta = tipoConta;
@@ -41,6 +49,7 @@ public class ExercicioConstrutores {
         imprimir();
     }
 
+    // Construtor 3 - titular, tipoConta e saldo
     public ExercicioConstrutores(String titular, String tipoConta, double saldo) {
         this.titular = titular;
         this.tipoConta = tipoConta;
@@ -60,7 +69,7 @@ public class ExercicioConstrutores {
         imprimir();
     }
 
-
+    // Construtor 4 - titular, tipoConta, saldo e limite
     public ExercicioConstrutores(String titular, String tipoConta, double saldo, double limite) {
         this.titular = titular;
         this.tipoConta = tipoConta;
@@ -69,13 +78,19 @@ public class ExercicioConstrutores {
         imprimir();
     }
 
-
-
+    // Método main - testando os construtores
     public static void main(String[] args) {
+// Cria uma conta só com o titular (usa Construtor 1)
         ExercicioConstrutores exercicioConstrutores = new ExercicioConstrutores("matheus");
+
+        // Cria uma conta com titular e tipoConta (Construtor 2)
         ExercicioConstrutores exercicioConstrutores1 = new ExercicioConstrutores("matheus", "corrente");
-        ExercicioConstrutores exercicioConstrutores2 = new ExercicioConstrutores("matheus", "corrente",2000 );
-        ExercicioConstrutores exercicioConstrutores3 = new ExercicioConstrutores("matheus", "corrente",2000,10000);
+
+        // Cria uma conta com titular, tipoConta e saldo (Construtor 3)
+        ExercicioConstrutores exercicioConstrutores2 = new ExercicioConstrutores("matheus", "corrente", 2000);
+
+        // Cria uma conta com todos os atributos definidos (Construtor 4)
+        ExercicioConstrutores exercicioConstrutores3 = new ExercicioConstrutores("matheus", "corrente", 2000, 10000);
     }
 
 }
